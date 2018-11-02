5ba289cc6427849cc8641fd2d22bc504724c1891
Edward Mwangi <edmwangi@microsoft.com>
2018-11-01 19:08:54 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+/** routes **/
+app.get('/', (req, res) => res.send('Home!'));
+
+// upload
+app.post('/api/image/upload', (req, res) => {
+    if (Object.keys(req.files).length == 0) {
+        return res.status(400).send('No files were uploaded.');
+      }
+      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+      let sampleFile = req.files.sampleFile;
+      console.log(sampleFile.name);
+      // Use the mv() method to place the file somewhere on your server
+      sampleFile.mv('./uploads/file.jpg', function(err) {
+        if (err)
+          return res.status(500).send(err);
+
+        res.send('File uploaded!');
+      });
+});
+
+// begin server
+app.listen(3000, () => {
+    console.log('server running...');
+});
